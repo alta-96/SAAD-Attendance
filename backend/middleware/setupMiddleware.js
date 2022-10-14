a327424bb51d74ebb4a637a34d0ff082c894fc75
Alex Ashley <alex.ashley@3squared.com>
2022-10-14 14:28:43 +0100

    restructure middleware setup

--- backend/middleware/setupMiddleware.js
+++ backend/middleware/setupMiddleware.js
@@ -1,14 +1,23 @@
 ﻿module.exports = (app) => {
     require("dotenv").config();
-    const bodyParser = require('body-parser');
-    const morgan = require('morgan');
     
+    configureBodyParser(app);
+    configureMorgan(app);
+    
+    app.disable('etag'); // prevents returning 304 no-change state and returns 200 instead...
+}
+
+function configureMorgan(app) {
+    const morgan = require('morgan');
+
     if (process.env.MODE === 'DEVELOP') {
-        app.use(morgan('common'));    
+        app.use(morgan('common'));
     } else {
         app.use(morgan('tiny'));
     }
-    
+}
+
+function configureBodyParser(app) {
+    const bodyParser = require('body-parser');
     app.use(bodyParser.json());
-    app.disable('etag');
 }
